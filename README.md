6a8b7515a26e93f9ca9f68eac5521e136ed434a2
Torkild U. Resheim <tur@itema.no>
2023-06-15 09:00:06 +0200

    Initial contribution with proof of concept (#1)

---
+++ README.md
@@ -0,0 +1,87 @@
+
+# DA OpenTelemetry Agent and Configuration Service
+
+This project includes a web service with a Representational State Transfer (REST) Application Programming Interface (API). The web service is designed to manage OpenTelemetry Sampler Configurations.
+
+## Motivation
+
+By creating a remotely controlled agent, we can dynamically change its behavior based on the current needs. For example, we could adjust sampling rates, enable or disable certain types of telemetry, or update configuration settings, all without having to redeploy or restart our applications.
+
+## Components
+
+### OpenTelemetry Configuration Service
+
+The _OpenTelemetry Configuration Service_ is a component of this project that keeps track of different sampler configurations. The sampler is a part of the OpenTelemetry SDK that determines whether a given span should be sampled and processed further. The configurations define the conditions under which a span is considered for sampling.
+
+This service exposes a RESTful API that allows clients to interact with it. The API provides a programmatic interface to the service, allowing software clients to interact with the service.
+
+You can use HTTP methods like GET, POST, PUT, DELETE, etc., to interact with the service via the REST API. For instance, you can retrieve current configurations, create new configurations, update existing ones, or delete configurations.
+
+### OpenTelemetry Java Agent Extension
+
+The project also includes an extension to the standard OpenTelemetry Java agent. This extension enables the Java agent to dynamically change the sampler implementation and its configuration.
+
+The OpenTelemetry Java agent is a tool that automatically instruments your Java application to track transactions and report metrics. Our extension enhances this agent's capabilities by allowing it to switch between different sampler implementations and configurations on the fly.
+
+This means that you can adjust your sampling strategy during runtime without having to stop and restart your application. It adds a great deal of flexibility to your observability strategy and can help you adapt to changing system dynamics or diagnostic needs.
+
+## Usage
+
+A minimum configuration for the application side is as follows:
+
+```shell
+  -javaagent:da-opentelemetry-javaagent.jar \
+  -Dotel.service.name="my-service-name" \
+  -Dotel.traces.sampler="dynamic" \
+  -Dotel.configuration.service.url="http://otel-configuration-service-url" \
+```
+
+Notice that `otel.traces.sampler` must be set to `dynamic` in order for this sampler to be used.
+
+## Local testing
+
+In order to test this setup, first start a Jaeger instance by calling `jaeger.sh` found in the root folder. This will start a new instance in Docker and expose ports 4317 and 16686. Having another instance at `localhost` with the same ports exposed is also fine.
+
+Now run `build-and-test.sh`. This will build the agent and the service, run the tests and start the service using the built agent.
+
+Since the configuration service is not started when it's being instrumented, obtaining the remote configuration will fail and defaults will be used. You will see something like this in the log:
+
+```
+opentelemetry-javaagent - version: 1.27.0-SNAPSHOT
+Could not connect to OTEL Configuration Service at http://localhost:8080, using default sampler "always_off".
+```
+
+If the OpenTelemetry Configuration Service is available, but does not contain a configuration for the agent, the agent will register itself. You can see this in the log as:
+
+```
+Self-registered as "da-otel-agent-service" at the OTEL Configuration Service
+```
+
+Now getting the available configurations from the service will yield:
+
+```
+curl -s -X GET http://localhost:8080/agent-configuration | jq
+[
+  {
+    "serviceName": "da-otel-agent-service",
+    "sampleRatio": 0,
+    "sampler": "always_off"
+  }
+]
+```
+
+In order to test the agent and the service you can execute the following to turn on sampling:
+
+```bash
+curl -X POST http://localhost:8080/agent-configuration \
+  -H 'Content-Type: application/json' \
+  -d '{"serviceName":"da-otel-agent-service", "sampler":"always_on"}'
+```
+
+The `DynamicSamplerProvicder` in the _OpenTelemetry Configuration Service_ will now respond with the following log message:
+
+```
+Changing sampler to AlwaysOnSampler
+```
+
+The `DynamicSamplerProvider` will poll the service at regular intervals (currently each 5s) to check for updated configurations. You should be able to see this in the Jaeger UI.
---
+++ service/src/test/java/no/domstol/otel/agent/service/AgentConfigurationControllerTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright Domstoladministrasjonen, Norway
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package no.domstol.otel.agent.service;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class AgentConfigurationControllerTest {
+
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    private AgentConfigurationController agentConfigurationController;
+
+    private static ConcurrentMap<String, DynamicAgentConfiguration> configurations;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(agentConfigurationController).build();
+        configurations = new ConcurrentHashMap<>();
+        agentConfigurationController.setConfigurations(configurations);
+    }
+
+    @Test
+    public void testAddAgentConfiguration() throws Exception {
+        DynamicAgentConfiguration agentConfiguration = new DynamicAgentConfiguration();
+        agentConfiguration.setServiceName("testAgent");
+        configurations.put(agentConfiguration.getServiceName(), agentConfiguration);
+
+        mockMvc.perform(post("/agent-configuration")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(agentConfiguration)))
+                .andExpect(status().isOk());
+
+        // Verify that the configuration has been added
+        assert(configurations.containsKey("testAgent"));
+    }
+
+    @Test
+    public void testGetAgentConfiguration() throws Exception {
+        DynamicAgentConfiguration agentConfiguration = new DynamicAgentConfiguration();
+        agentConfiguration.setServiceName("testAgent");
+        configurations.put(agentConfiguration.getServiceName(), agentConfiguration);
+
+        mockMvc.perform(get("/agent-configuration/testAgent")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.serviceName", is("testAgent")));
+    }
+
+    @Test
+    public void testEditAgentConfiguration() throws Exception {
+        DynamicAgentConfiguration agentConfiguration = new DynamicAgentConfiguration();
+        agentConfiguration.setServiceName("testAgent");
+        configurations.put(agentConfiguration.getServiceName(), agentConfiguration);
+
+        DynamicAgentConfiguration newConfiguration = new DynamicAgentConfiguration();
+        newConfiguration.setServiceName("testAgent");
+        newConfiguration.setSampleRatio(0.5);
+
+        mockMvc.perform(put("/agent-configuration/testAgent")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(newConfiguration)))
+                .andExpect(status().isOk());
+
+        // Verify the configuration has been updated
+        assert (configurations.get("testAgent").getSampleRatio() == 0.5);
+    }
+
+    @Test
+    public void testGetAllAgentConfigurations() throws Exception {
+        DynamicAgentConfiguration agentConfiguration1 = new DynamicAgentConfiguration();
+        agentConfiguration1.setServiceName("testAgent1");
+        configurations.put(agentConfiguration1.getServiceName(), agentConfiguration1);
+
+        DynamicAgentConfiguration agentConfiguration2 = new DynamicAgentConfiguration();
+        agentConfiguration2.setServiceName("testAgent2");
+        configurations.put(agentConfiguration2.getServiceName(), agentConfiguration2);
+
+        mockMvc.perform(get("/agent-configuration")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].serviceName", is("testAgent1")))
+                .andExpect(jsonPath("$[1].serviceName", is("testAgent2")));
+    }
+
+    // A helper method to turn an object into a JSON string
+    private static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
