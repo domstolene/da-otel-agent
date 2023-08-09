dc3f663fe132f4c1015afab2cb090daba9bce1db
Torkild U. Resheim <tur@itema.no>
2023-08-09 13:56:43 +0200

    Clean up build and test script

--- build-and-test.sh
+++ build-and-test.sh
@@ -9,8 +9,4 @@ java \
   -Dotel.metrics.exporter="none" \
   -Dotel.traces.sampler="dynamic" \
   -Dotel.service.name="da-otel-agent-service" \
-  -Dotel.configuration.service.url="http://localhost:8080" \
-  -Dotel.configuration.service.file=$current_dir/extension/src/test/resources/traces-configuration.yaml \
-  -Dotel.configuration.service.api.key="0DAE3387-4CDA-417D-B084-53BEC56B7B55" \
-  -Dotel.javaagent.logging=application \
   -jar $current_dir/service/build/libs/service.jar
