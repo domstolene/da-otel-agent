110bac3d6b723dcde05da3be226ef767899036ee
Torkild U. Resheim <tur@itema.no>
2025-03-20 13:38:29 +0100

    Add frontend scaffolding and configure Spring Tool Suite launch config

---
+++ frontend/src/main/java/no/domstol/otel/agent/frontend/AgentConfigurationServiceFrontend.java
@@ -0,0 +1,13 @@
+package no.domstol.otel.agent.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentConfigurationServiceFrontend {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentConfigurationServiceFrontend.class, args);
+	}
+
+}
